07eff002470243ce9d19853eea132fcb86fc2ac4
David Crook <idcrook@users.noreply.github.com>
2020-03-08 17:36:46 -0600

    struct before ennum refactor

---
+++ ch6/enums/src/main.rs
@@ -0,0 +1,27 @@
+#![allow(unused_variables)]
+fn main() {
+    enum IpAddrKind {
+        V4,
+        V6,
+    }
+
+    struct IpAddr {
+        kind: IpAddrKind,
+        address: String,
+    }
+
+    let home = IpAddr {
+        kind: IpAddrKind::V4,
+        address: String::from("127.0.0.1"),
+    };
+
+    let loopback = IpAddr {
+        kind: IpAddrKind::V6,
+        address: String::from("::1"),
+    };
+
+    fn route(ip_kind: IpAddrKind) {}
+
+    route(IpAddrKind::V4);
+    route(IpAddrKind::V6);
+}
