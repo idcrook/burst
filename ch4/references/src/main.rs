091e6aff93b696d20e8a5e4574b7975568d2269e
David Crook <idcrook@users.noreply.github.com>
2020-03-08 14:13:56 -0600

    hello references

---
+++ ch4/references/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let s1 = String::from("hello");
+
+    let len = calculate_length(&s1);
+
+    println!("The length of '{}' is {}.", s1, len);
+}
+
+// s is a reference to a String
+fn calculate_length(s: &String) -> usize {
+    s.len()
+} // Here, s goes out of scope. But because it does not have ownership of what
+  // it refers to, nothing happens.
