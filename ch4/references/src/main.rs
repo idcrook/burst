0be739486fb160d284e68db76ee7eafba9c60618
David Crook <idcrook@users.noreply.github.com>
2020-03-08 14:25:39 -0600

    mutable and immutable borrows

--- ch4/references/src/main.rs
+++ ch4/references/src/main.rs
@@ -4,10 +4,44 @@ fn main() {
     let len = calculate_length(&s1);
 
     println!("The length of '{}' is {}.", s1, len);
-}
 
+    let mut s = String::from("hello");
+
+    change(&mut s);
+    println!("{}", s);
+
+    // let r1 = &mut s;
+    // let r2 = &mut s;
+    // // cannot borrow as mutable more than one at a time
+    // println!("{}, {}", r1, r2);
+
+    let mut s = String::from("hello");
+
+    // let r1 = &s; // no problem
+    // let r2 = &s; // no problem
+    // let r3 = &mut s; // BIG PROBLEM
+    //                  // cannot mix immutable and mutable borrows
+    // println!("{}, {}, and {}", r1, r2, r3);
+
+    let r1 = &s; // no problem
+    let r2 = &s; // no problem
+    println!("{} and {}", r1, r2);
+    // r1 and r2 are no longer used after this point
+
+    let r3 = &mut s; // no problem
+    println!("{}", r3);
+}
 // s is a reference to a String
 fn calculate_length(s: &String) -> usize {
     s.len()
 } // Here, s goes out of scope. But because it does not have ownership of what
   // it refers to, nothing happens.
+
+// // try to modify something being borrowed
+// fn change(some_string: &String) {
+//     some_string.push_str(", world");
+// }
+
+fn change(some_string: &mut String) {
+    some_string.push_str(", world");
+}
