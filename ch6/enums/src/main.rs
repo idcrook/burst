804be21f3ab4cb47e46341380df0749559f2826c
David Crook <idcrook@users.noreply.github.com>
2020-03-08 17:47:08 -0600

    method in enum

--- ch6/enums/src/main.rs
+++ ch6/enums/src/main.rs
@@ -1,16 +1,39 @@
+// #![allow(unused_variables)]
+// fn main() {
+//     enum Message {
+//         Quit,                       // no associated data
+//         Move { x: i32, y: i32 },    // anonymous struct in
+//         Write(String),              // single String
+//         ChangeColor(i32, i32, i32), // three i32 values
+//     }
+// }
+
+// #![allow(unused_variables)]
+// fn main() {
+// struct QuitMessage; // unit struct
+// struct MoveMessage {
+//     x: i32,
+//     y: i32,
+// }
+// struct WriteMessage(String); // tuple struct
+// struct ChangeColorMessage(i32, i32, i32); // tuple struct
+// }
+
 #![allow(unused_variables)]
 fn main() {
-    enum IpAddrKind {
-        V4,
-        V6,
+    enum Message {
+        Quit,
+        Move { x: i32, y: i32 },
+        Write(String),
+        ChangeColor(i32, i32, i32),
     }
 
-    enum IpAddr {
-        V4(u8, u8, u8, u8),
-        V6(String),
+    impl Message {
+        fn call(&self) {
+            // method body would be defined here
+        }
     }
 
-    let home = IpAddr::V4(127, 0, 0, 1);
-
-    let loopback = IpAddr::V6(String::from("::1"));
+    let m = Message::Write(String::from("hello"));
+    m.call();
 }
