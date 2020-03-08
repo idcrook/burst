a4794092a11702f0b6c59e3e8c2bc2432785a2e7
David Crook <idcrook@users.noreply.github.com>
2020-03-08 17:41:44 -0600

    enum values

--- ch6/enums/src/main.rs
+++ ch6/enums/src/main.rs
@@ -5,23 +5,12 @@ fn main() {
         V6,
     }
 
-    struct IpAddr {
-        kind: IpAddrKind,
-        address: String,
+    enum IpAddr {
+        V4(u8, u8, u8, u8),
+        V6(String),
     }
 
-    let home = IpAddr {
-        kind: IpAddrKind::V4,
-        address: String::from("127.0.0.1"),
-    };
+    let home = IpAddr::V4(127, 0, 0, 1);
 
-    let loopback = IpAddr {
-        kind: IpAddrKind::V6,
-        address: String::from("::1"),
-    };
-
-    fn route(ip_kind: IpAddrKind) {}
-
-    route(IpAddrKind::V4);
-    route(IpAddrKind::V6);
+    let loopback = IpAddr::V6(String::from("::1"));
 }
