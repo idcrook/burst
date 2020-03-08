9e2ef770fea530e7b80e051e55169bea93984f95
David Crook <idcrook@users.noreply.github.com>
2020-03-08 16:33:53 -0600

    moving onto second first_word

--- ch4/slices/src/main.rs
+++ ch4/slices/src/main.rs
@@ -1,22 +1,47 @@
 fn main() {
-    let mut s = String::from("hello world");
+    let s = String::from("hello world");
 
-    let word = first_word(&s); // word will get the value 5
+    let hello = &s[0..5];
+    let world = &s[6..11];
 
-    s.clear(); // this empties the String, making it equal to ""
+    println!("{} {}", hello, world);
 
-    // word still has the value 5 here, but there's no more string that
-    // we could meaningfully use the value 5 with. word is now totally invalid!
+    let s = String::from("hello");
+
+    let slice = &s[0..2];
+    let slice = &s[..2];
+
+    println!("{}", slice);
+
+    let len = s.len();
+
+    let slice = &s[3..len];
+    let slice = &s[3..];
+    let slice = &s[0..len];
+    let slice = &s[..];
+    println!("{}", slice);
 }
 
-fn first_word(s: &String) -> usize {
+// fn first_word(s: &String) -> usize {
+//     let bytes = s.as_bytes();
+
+//     for (i, &item) in bytes.iter().enumerate() {
+//         if item == b' ' {
+//             return i;
+//         }
+//     }
+
+//     s.len()
+// }
+
+fn first_word(s: &String) -> &str {
     let bytes = s.as_bytes();
 
     for (i, &item) in bytes.iter().enumerate() {
         if item == b' ' {
-            return i;
+            return &s[0..i];
         }
     }
 
-    s.len()
+    &s[..]
 }
