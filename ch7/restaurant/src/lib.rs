1f7dd4bc381a5e7bb74d960663f5344930fce73f
David Crook <idcrook@users.noreply.github.com>
2020-03-08 21:04:13 -0600

    use use

--- ch7/restaurant/src/lib.rs
+++ ch7/restaurant/src/lib.rs
@@ -1,39 +1,24 @@
-#![allow(unused_variables)]
-fn main() {
-    mod back_of_house {
-        pub enum Appetizer {
-            Soup,
-            Salad,
-        }
-        pub struct Breakfast {
-            pub toast: String,
-            seasonal_fruit: String,
-        }
-
-        impl Breakfast {
-            pub fn summer(toast: &str) -> Breakfast {
-                Breakfast {
-                    toast: String::from(toast),
-                    seasonal_fruit: String::from("peaches"),
-                }
-            }
-        }
-    }
-
-    pub fn eat_at_restaurant() {
-        // Order a breakfast in the summer with Rye toast
-        let mut meal = back_of_house::Breakfast::summer("Rye");
-        // Change our mind about what bread we'd like
-        meal.toast = String::from("Wheat");
-        println!("I'd like {} toast please", meal.toast);
-
-        // // The next line won't compile if we uncomment it; we're not allowed
-        // // to see or modify the seasonal fruit that comes with the meal
-        // meal.seasonal_fruit = String::from("blueberries");
-    }
-
-    pub fn eat_at_restaurant_enum() {
-        let order1 = back_of_house::Appetizer::Soup;
-        let order2 = back_of_house::Appetizer::Salad;
+mod front_of_house {
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
     }
 }
+
+// // bring a module into scope
+// use crate::front_of_house::hosting;
+
+// unidiomatic since it is local
+// use crate::front_of_house::hosting::add_to_waitlist;
+
+// re-exporting with pub use
+pub use crate::front_of_house::hosting;
+//  external code can now call the add_to_waitlist function using
+//  hosting::add_to_waitlist
+
+pub fn eat_at_restaurant() {
+    // use a relative path
+    hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
+}
+fn main() {}
