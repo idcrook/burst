12c6937794a2d4db13a8b6d45afadceafa36c3a6
David Crook <idcrook@users.noreply.github.com>
2020-03-08 21:42:51 -0600

    add a std::

--- ch7/restaurant/src/lib.rs
+++ ch7/restaurant/src/lib.rs
@@ -1,8 +1,11 @@
+use std::vec::Vec;
 mod front_of_house;
 
 pub use crate::front_of_house::hosting;
 
 pub fn eat_at_restaurant() {
+    let v: Vec<i32> = Vec::new();
+
     hosting::add_to_waitlist();
     hosting::add_to_waitlist();
     hosting::add_to_waitlist();
