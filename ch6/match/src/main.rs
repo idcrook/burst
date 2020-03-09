f777df463d80fbb8abcb3e1c3f02127607b5635b
David Crook <idcrook@users.noreply.github.com>
2020-03-08 18:52:04 -0600

    catchall with _

--- ch6/match/src/main.rs
+++ ch6/match/src/main.rs
@@ -1,27 +1,30 @@
 #![allow(unused_variables)]
 fn main() {
-    #[derive(Debug)]
-    enum UsState {
-        Alabama,
-        Alaska,
-    }
-
-    enum Coin {
-        Penny,
-        Nickel,
-        Dime,
-        Quarter(UsState),
-    }
-
-    fn value_in_cents(coin: Coin) -> u8 {
-        match coin {
-            Coin::Penny => 1,
-            Coin::Nickel => 5,
-            Coin::Dime => 10,
-            Coin::Quarter(state) => {
-                println!("State quarter from {:?}!", state);
-                25
-            }
+    fn plus_one(x: Option<i32>) -> Option<i32> {
+        match x {
+            None => None,
+            Some(i) => Some(i + 1),
         }
     }
+
+    // // pattern `None` not covered
+    // fn plus_one(x: Option<i32>) -> Option<i32> {
+    //     match x {
+    //         Some(i) => Some(i + 1),
+    //     }
+    // }
+
+    let some_u8_value = 0u8;
+    match some_u8_value {
+        1 => println!("one"),
+        3 => println!("three"),
+        5 => println!("five"),
+        7 => println!("seven"),
+        8u8..=std::u8::MAX => println!("others"), //_ => (),
+        _ => (),
+    }
+
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
 }
