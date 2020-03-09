44a637aa92338ad3f487eaa2ea4bd80156539601
David Crook <idcrook@users.noreply.github.com>
2020-03-08 18:58:07 -0600

    if let for enums

---
+++ ch6/if_let/src/main.rs
@@ -0,0 +1,38 @@
+#![allow(unused_variables)]
+fn main() {
+    #[derive(Debug)]
+    enum UsState {
+        Alabama,
+        Alaska,
+    }
+
+    enum Coin {
+        Penny,
+        Nickel,
+        Dime,
+        Quarter(UsState),
+    }
+    let coin = Coin::Penny;
+    // let mut count = 0;
+    // match coin {
+    //     Coin::Quarter(state) => println!("State quarter from {:?}!", state),
+    //     _ => count += 1,
+    // }
+
+    let mut count = 0;
+    if let Coin::Quarter(state) = coin {
+        println!("State quarter from {:?}!", state);
+    } else {
+        count += 1;
+    }
+
+    let some_u8_value = Some(0u8);
+    // match some_u8_value {
+    //     Some(3) => println!("three"),
+    //     _ => (),
+    // }
+
+    if let Some(3) = some_u8_value {
+        println!("three");
+    }
+}
