a72af7adda9e06235f75d4f0f5297dfaf3befd1f
David Crook <idcrook@users.noreply.github.com>
2020-03-10 13:25:15 -0600

    example with closures and .unwrap on Result

--- ch9/recoverables/src/main.rs
+++ ch9/recoverables/src/main.rs
@@ -1,17 +1,37 @@
 #![allow(unused_variables)]
 use std::fs::File;
+use std::io::ErrorKind;
 
 fn main() {
-    // let f: u32 = File::open("hello.txt"); // use bogus type to have compiler inform on the return type
-    // expected `u32`, found enum `std::result::Result`
-    // -> io::Result<File>
+    // // shortcut to panic if Error
+    // let f = File::open("hello.txt").unwrap();
+    //thread 'main' panicked at 'called `Result::unwrap()` on an `Err` value:
+    // Os { code: 2, kind: NotFound, message: "No such file or directory" }',
+    // src/libcore/result.rs:1188:5
+
     let f = File::open("hello.txt");
 
     let f = match f {
         Ok(file) => file,
-        Err(error) => panic!("Problem opening the file: {:?}", error),
-        // thread 'main' panicked at 'Problem opening the file: Os { code: 2,
-        // kind: NotFound, message: "No such file or directory" }',
-        // src/main.rs:12:23
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("Problem creating the file: {:?}", e),
+            },
+            other_error => panic!("Problem opening the file: {:?}", other_error),
+        },
     };
 }
+
+// // version using closures
+// fn main() {
+//     let f = File::open("hello.txt").unwrap_or_else(|error| {
+//         if error.kind() == ErrorKind::NotFound {
+//             File::create("hello.txt").unwrap_or_else(|error| {
+//                 panic!("Problem creating the file: {:?}", error);
+//             })
+//         } else {
+//             panic!("Problem opening the file: {:?}", error);
+//         }
+//     });
+// }
