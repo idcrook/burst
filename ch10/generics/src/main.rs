f4d5efe6ec3270bf6e79fb015cb7e8be71192b01
David Crook <idcrook@users.noreply.github.com>
2020-03-10 16:31:28 -0600

    does not compile: try templated / generic

--- ch10/generics/src/main.rs
+++ ch10/generics/src/main.rs
@@ -1,20 +1,12 @@
-fn largest_i32(list: &[i32]) -> i32 {
+fn largest<T>(list: &[T]) -> T {
     let mut largest = list[0];
 
     for &item in list.iter() {
         if item > largest {
-            largest = item;
-        }
-    }
+            // error[E0369]: binary operation `>` cannot be applied to type `T`
+            //   = note: `T` might need a bound for `std::cmp::PartialOrd`
 
-    largest
-}
-
-fn largest_char(list: &[char]) -> char {
-    let mut largest = list[0];
-
-    for &item in list.iter() {
-        if item > largest {
+            //
             largest = item;
         }
     }
@@ -25,64 +17,11 @@ fn largest_char(list: &[char]) -> char {
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
 
-    let result = largest_i32(&number_list);
+    let result = largest(&number_list);
     println!("The largest number is {}", result);
 
     let char_list = vec!['y', 'm', 'a', 'q'];
 
-    let result = largest_char(&char_list);
+    let result = largest(&char_list);
     println!("The largest char is {}", result);
 }
-
-// fn largest(list: &[i32]) -> i32 {
-//     let mut largest = list[0];
-
-//     for &item in list {
-//         if item > largest {
-//             largest = item;
-//         }
-//     }
-
-//     largest
-// }
-
-// fn main() {
-//     let number_list = vec![34, 50, 25, 100, 65];
-
-//     let result = largest(&number_list);
-//     println!("The largest number is {}", result);
-//     assert_eq!(result, 100);
-
-//     let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
-
-//     let result = largest(&number_list);
-//     println!("The largest number is {}", result);
-//     assert_eq!(result, 6000);
-// }
-
-// fn main() {
-//     let number_list = vec![34, 50, 25, 100, 65];
-
-//     let mut largest = number_list[0];
-
-//     for number in number_list {
-//         if number > largest {
-//             largest = number;
-//         }
-//     }
-
-//     println!("The largest number is {}", largest);
-//     //assert_eq!(largest, 100);
-
-//     let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
-
-//     let mut largest = number_list[0];
-
-//     for number in number_list {
-//         if number > largest {
-//             largest = number;
-//         }
-//     }
-
-//     println!("The largest number is {}", largest);
-// }
