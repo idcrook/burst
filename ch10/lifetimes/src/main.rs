c3c3a769d81995bb59136b4570c169e9bd13ad65
David Crook <idcrook@users.noreply.github.com>
2020-03-10 19:09:35 -0600

    need generic lifetime parameter

---
+++ ch10/lifetimes/src/main.rs
@@ -0,0 +1,26 @@
+fn longest(x: &str, y: &str) -> &str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
+fn main() {
+    let r;
+
+    {
+        let x = 5;
+        // error[E0597]: `x` does not live long enough
+        r = &x;
+        //  ^^ borrowed value does not live long enough
+        println!("r: {}", r);
+    }
+
+    //println!("r: {}", r);
+    let string1 = String::from("abcd");
+    let string2 = "xyz";
+
+    let result = longest(string1.as_str(), string2);
+    println!("The longest string is {}", result);
+}
