ce65a7220d85915bfb50b4ed6d3bca91c1bfd228
David Crook <idcrook@users.noreply.github.com>
2020-03-12 12:36:35 -0600

    implement lazy evaluation struct

--- ch13/closures/src/main.rs
+++ ch13/closures/src/main.rs
@@ -2,11 +2,37 @@
 use std::thread;
 use std::time::Duration;
 
-// fn simulated_expensive_calculation(intensity: u32) -> u32 {
-//     println!("calculating slowly...");
-//     thread::sleep(Duration::from_secs(2));
-//     intensity
-// }
+// implement memoization / lazy evaluation
+struct Cacher<T>
+where
+    T: Fn(u32) -> u32, // Fn trait
+{
+    calculation: T,
+    value: Option<u32>,
+}
+
+impl<T> Cacher<T>
+where
+    T: Fn(u32) -> u32,
+{
+    fn new(calculation: T) -> Cacher<T> {
+        Cacher {
+            calculation,
+            value: None,
+        }
+    }
+
+    fn value(&mut self, arg: u32) -> u32 {
+        match self.value {
+            Some(v) => v,
+            None => {
+                let v = (self.calculation)(arg);
+                self.value = Some(v);
+                v
+            }
+        }
+    }
+}
 
 fn main() {
     let simulated_user_specified_value = 10;
