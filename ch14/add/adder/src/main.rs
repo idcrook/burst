9ac25512e5e2214d1cc574889ad6ac8be55fe299
David Crook <idcrook@users.noreply.github.com>
2020-03-12 16:48:59 -0600

    workspace example for bin and lib crates

---
+++ ch14/add/adder/src/main.rs
@@ -0,0 +1,15 @@
+#![allow(unused_imports)]
+use add_one;
+use add_two;
+use rand;
+
+fn main() {
+    let num = 10;
+    println!(
+        "Hello, world! {} plus one is {}!",
+        num,
+        add_one::add_one(num)
+    );
+
+    println!("{} plus two is {}!", num, add_two::add_two(num));
+}
