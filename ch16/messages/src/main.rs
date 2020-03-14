60380523acf49120dd9a7c53b3d6ca9cc32563c1
David Crook <idcrook@users.noreply.github.com>
2020-03-14 17:12:42 -0600

    create multiple producers by cloning the receiver

--- ch16/messages/src/main.rs
+++ ch16/messages/src/main.rs
@@ -5,6 +5,7 @@ use std::time::Duration;
 fn main() {
     let (tx, rx) = mpsc::channel();
 
+    let tx1 = mpsc::Sender::clone(&tx);
     thread::spawn(move || {
         let vals = vec![
             String::from("hi"),
@@ -12,13 +13,27 @@ fn main() {
             String::from("the"),
             String::from("thread"),
         ];
+
         for val in vals {
-            tx.send(val).unwrap(); // unwrap will cause a panic upon an error
+            tx1.send(val).unwrap();
             thread::sleep(Duration::from_secs(1));
         }
     });
 
-    // can treat rx like an iterator
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("more"),
+            String::from("messages"),
+            String::from("for"),
+            String::from("you"),
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
     for received in rx {
         println!("Got: {}", received);
     }
