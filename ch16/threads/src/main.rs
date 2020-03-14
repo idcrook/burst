b037bea47e9d850c7542d4dca6c3bccc6bbb8021
David Crook <idcrook@users.noreply.github.com>
2020-03-14 16:56:55 -0600

    thread state safety

--- ch16/threads/src/main.rs
+++ ch16/threads/src/main.rs
@@ -16,4 +16,15 @@ fn main() {
     }
 
     handle.join().unwrap();
+
+    let v = vec![1, 2, 3];
+
+    //let handle = thread::spawn(|| {
+    let handle = thread::spawn(move || {
+        println!("Here's a vector: {:?}", v);
+    });
+
+    // drop(v); // oh no!
+
+    handle.join().unwrap();
 }
