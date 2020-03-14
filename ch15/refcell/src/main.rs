b3596cdf640434eff350646974169e704a403c56
David Crook <idcrook@users.noreply.github.com>
2020-03-14 14:45:43 -0600

    nest Rc<RefCell<T>> to get a mutable value
    that can have multiple owners andis mutable

--- ch15/refcell/src/main.rs
+++ ch15/refcell/src/main.rs
@@ -1,2 +1,25 @@
-#![allow(unused_variables)]
-fn main() {}
+#[derive(Debug)]
+enum List {
+    Cons(Rc<RefCell<i32>>, Rc<List>),
+    Nil,
+}
+
+use crate::List::{Cons, Nil};
+use std::cell::RefCell;
+use std::rc::Rc;
+
+fn main() {
+    let value = Rc::new(RefCell::new(5));
+
+    // .clones are use so that multiple owners can have access to inner value
+    let a = Rc::new(Cons(Rc::clone(&value), Rc::new(Nil)));
+
+    let b = Cons(Rc::new(RefCell::new(6)), Rc::clone(&a));
+    let c = Cons(Rc::new(RefCell::new(10)), Rc::clone(&a));
+
+    *value.borrow_mut() += 10;
+
+    println!("a after = {:?}", a);
+    println!("b after = {:?}", b);
+    println!("c after = {:?}", c);
+}
