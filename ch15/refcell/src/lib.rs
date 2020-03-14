4651004be7238aae7ac0e8cae51ff727e0778e52
David Crook <idcrook@users.noreply.github.com>
2020-03-14 14:45:23 -0600

    switch back to working code

--- ch15/refcell/src/lib.rs
+++ ch15/refcell/src/lib.rs
@@ -61,6 +61,16 @@ mod tests {
         }
     }
 
+    // impl Messenger for MockMessenger {
+    //     fn send(&self, message: &str) {
+    //         let mut one_borrow = self.sent_messages.borrow_mut();
+    //         let mut two_borrow = self.sent_messages.borrow_mut(); // panicked at 'already borrowed: BorrowMutError',
+
+    //         one_borrow.push(String::from(message));
+    //         two_borrow.push(String::from(message));
+    //     }
+    // }
+
     #[test]
     fn it_sends_an_over_75_percent_warning_message() {
         let mock_messenger = MockMessenger::new();
