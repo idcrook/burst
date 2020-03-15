0c8ad93e641b5222c9c0255b7e0a581c4763d8ef
David Crook <idcrook@users.noreply.github.com>
2020-03-14 20:23:00 -0600

    checkin pattern syntaxes

--- ch18/patternsyntax/src/main.rs
+++ ch18/patternsyntax/src/main.rs
@@ -1,3 +1,26 @@
+#![allow(unused_variables)]
+
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+enum Color {
+    Rgb(i32, i32, i32),
+    Hsv(i32, i32, i32),
+}
+
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(Color),
+}
+
+fn foo(_: i32, y: i32) {
+    println!("This code only uses the y parameter: {}", y);
+}
+
 fn main() {
     let x = 1;
 
@@ -48,10 +71,10 @@ fn main() {
     }
 
     // destructuring to break apart values
-    struct Point {
-        x: i32,
-        y: i32,
-    }
+    // struct Point {
+    //     x: i32,
+    //     y: i32,
+    // }
 
     let p = Point { x: 0, y: 7 };
 
@@ -68,5 +91,145 @@ fn main() {
         Point { x, y: 0 } => println!("On the x axis at {}", x),
         Point { x: 0, y } => println!("On the y axis at {}", y),
         Point { x, y } => println!("On neither axis: ({}, {})", x, y),
+    }
+
+    // destructuring enums
+    let msg = Message::Quit;
+    // let msg = Message::ChangeColor(0, 160, 255);
+
+    match msg {
+        Message::Quit => println!("The Quit variant has no data to destructure."),
+        Message::Move { x, y } => {
+            println!("Move in the x direction {} and in the y direction {}", x, y);
+        }
+        Message::Write(text) => println!("Text message: {}", text),
+        // Message::ChangeColor(r, g, b) => {
+        //     println!("Change the color to red {}, green {}, and blue {}", r, g, b)
+        // }
+        _ => println!("Probably was ChangeColor"),
+    }
+
+    // Destructuring Nested Structs and Enums
+    let msg = Message::ChangeColor(Color::Hsv(0, 160, 255));
+
+    match msg {
+        Message::ChangeColor(Color::Rgb(r, g, b)) => {
+            println!("Change the color to red {}, green {}, and blue {}", r, g, b)
+        }
+        Message::ChangeColor(Color::Hsv(h, s, v)) => println!(
+            "Change the color to hue {}, saturation {}, and value {}",
+            h, s, v
+        ),
+        _ => (),
+    }
+
+    // Destructuring Structs and Tuples
+    let ((feet, inches), Point { x, y }) = ((3, 10), Point { x: 3, y: -10 });
+
+    // Ignoring an Entire Value with _
+    foo(3, 4);
+
+    // Ignoring Parts of a Value with a Nested _
+    let mut setting_value = Some(5);
+    let new_setting_value = Some(10);
+
+    match (setting_value, new_setting_value) {
+        (Some(_), Some(_)) => {
+            println!("Can't overwrite an existing customized value");
+        }
+        _ => {
+            setting_value = new_setting_value;
+        }
+    }
+
+    println!("setting is {:?}", setting_value);
+
+    let numbers = (2, 4, 8, 16, 32);
+
+    match numbers {
+        (first, _, third, _, fifth) => println!("Some numbers: {}, {}, {}", first, third, fifth),
+    }
+
+    // Ignoring an Unused Variable by Starting Its Name with _
+    let _x = 5;
+    let y = 10;
+
+    let s = Some(String::from("Hello!"));
+
+    // if let Some(_s) = s { // error below on s: borrow of moved value s
+    // Using an underscore does not bind the value
+    if let Some(_) = s {
+        println!("found a string");
+    }
+
+    println!("{:?}", s);
+
+    // Ignoring Remaining Parts of a Value with ..
+    struct Point3D {
+        x: i32,
+        y: i32,
+        z: i32,
+    }
+
+    let origin = Point3D { x: 0, y: 0, z: 0 };
+
+    match origin {
+        Point3D { x, .. } => println!("x is {}", x),
+    }
+
+    // with a tuple
+    let numbers = (2, 4, 8, 16, 32);
+
+    match numbers {
+        (first, .., last) => {
+            println!("Some numbers: {}, {}", first, last);
+        }
+    }
+
+    // match numbers {
+    //     (.., second, ..) => println!("Some numbers: {}", second), // `..` can only be used once per tuple pattern
+    // }
+
+    // Extra Conditionals with Match Guards
+    let num = Some(4);
+
+    match num {
+        Some(x) if x < 5 => println!("less than five: {}", x),
+        Some(x) => println!("{}", x),
+        None => (),
+    }
+
+    let x = Some(5);
+    let y = 10;
+
+    match x {
+        Some(50) => println!("Got 50"),
+        Some(n) if n == y => println!("Matched, n = {}", n),
+        _ => println!("Default case, x = {:?}", x),
+    }
+
+    println!("at the end: x = {:?}, y = {}", x, y);
+
+    let x = 4;
+    let y = false;
+
+    match x {
+        4 | 5 | 6 if y => println!("yes"),
+        _ => println!("no"),
+    }
+
+    // @ Bindings
+    enum NMessage {
+        Hello { id: i32 },
+    }
+
+    let msg = NMessage::Hello { id: 5 };
+
+    match msg {
+        NMessage::Hello {
+            id: id_variable @ 3..=7,
+        } => println!("Found an id in range: {}", id_variable),
+        NMessage::Hello { id: 10..=12 } => println!("Found an id in another range"),
+        NMessage::Hello { id } => println!("Found some other id: {}", id),
     }
 }
