55d4e5f05942d34d00de400820afdd7da68aa195
David Crook <idcrook@users.noreply.github.com>
2020-03-14 19:44:17 -0600

    function parameters

--- ch18/matchplaces/src/main.rs
+++ ch18/matchplaces/src/main.rs
@@ -34,4 +34,11 @@ fn main() {
     }
 
     let (x, y, z) = (1, 2, 3);
+
+    let point = (3, 5);
+    print_coordinates(&point);
+}
+
+fn print_coordinates(&(x, y): &(i32, i32)) {
+    println!("Current location: ({}, {})", x, y);
 }
