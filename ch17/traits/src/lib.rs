9987054d7ce5a753f9c524fda8521ce836a7e5ff
David Crook <idcrook@users.noreply.github.com>
2020-03-14 18:46:30 -0600

    trait to resemble object-oriented morphologies

--- ch17/traits/src/lib.rs
+++ ch17/traits/src/lib.rs
@@ -1,8 +1,53 @@
 #![allow(unused_variables)]
+
+// Defining a Trait for Common Behavior
 pub trait Draw {
     fn draw(&self);
 }
 
 pub struct Screen {
+    // This vector is of type Box<dyn Draw>, which is a trait object; it’s a
+    // stand-in for any type inside a Box that implements the Draw trait.
     pub components: Vec<Box<dyn Draw>>,
+}
+
+impl Screen {
+    pub fn run(&self) {
+        for component in self.components.iter() {
+            component.draw();
+        }
+    }
+}
+
+// alternative struct with generic type parameter with trait bounds
+// If you’ll only ever have homogeneous collections, using generics and trait
+// bounds is preferable because the definitions will be monomorphized at
+// compile time to use the concrete types.
+
+// pub struct Screen<T: Draw> {
+//     pub components: Vec<T>,
+// }
+
+// impl<T> Screen<T>
+// where
+//     T: Draw,
+// {
+//     pub fn run(&self) {
+//         for component in self.components.iter() {
+//             component.draw();
+//         }
+//     }
+// }
+
+pub struct Button {
+    pub width: u32,
+    pub height: u32,
+    pub label: String,
+}
+
+// implementing the Trait
+impl Draw for Button {
+    fn draw(&self) {
+        // code to actually draw a button
+    }
 }
---
+++ ch17/traits/src/main.rs
@@ -0,0 +1,38 @@
+use traits::Draw;
+use traits::{Button, Screen};
+
+struct SelectBox {
+    width: u32,
+    height: u32,
+    options: Vec<String>,
+}
+
+impl Draw for SelectBox {
+    fn draw(&self) {
+        // code to actually draw a select box
+    }
+}
+
+fn main() {
+    let screen = Screen {
+        components: vec![
+            Box::new(SelectBox {
+                width: 75,
+                height: 10,
+                options: vec![
+                    String::from("Yes"),
+                    String::from("Maybe"),
+                    String::from("No"),
+                ],
+            }),
+            Box::new(Button {
+                width: 50,
+                height: 10,
+                label: String::from("OK"),
+            }),
+            // Box::new(String::from("Hi")), //  the trait bound `std::string::String: traits::Draw` is not satisfied
+        ],
+    };
+
+    screen.run();
+}
