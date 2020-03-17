5f92afcd5b67ae45a7dac377a683d8ea2ea07160
David Crook <idcrook@users.noreply.github.com>
2020-03-16 18:03:34 -0600

    p

---
+++ ch19/point/src/main.rs
@@ -0,0 +1,42 @@
+#![allow(unused_variables)]
+use std::fmt;
+
+struct Wrapper(Vec<String>);
+
+impl fmt::Display for Wrapper {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "[{}]", self.0.join(", "))
+    }
+}
+
+trait OutlinePrint: fmt::Display {
+    fn outline_print(&self) {
+        let output = self.to_string();
+        let len = output.len();
+        println!("{}", "*".repeat(len + 4));
+        println!("*{}*", " ".repeat(len + 2));
+        println!("* {} *", output);
+        println!("*{}*", " ".repeat(len + 2));
+        println!("{}", "*".repeat(len + 4));
+    }
+}
+
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+impl OutlinePrint for Point {}
+
+impl fmt::Display for Point {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "({}, {})", self.x, self.y)
+    }
+}
+
+fn main() {
+    // let w = Wrapper(vec![String::from("hello"), String::from("world")]);
+    // println!("w = {}", w);
+    // let s1: str = "Hello there!";
+    // let s2: str = "How's it going?";
+}
